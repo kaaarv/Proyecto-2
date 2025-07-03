ea1622cb9b9a628c65b4e5cba175e520321a73ad
Randy <randysteve500@gmail.com>
2025-07-02 21:24:00 -0600

    act

--- Multiplexor.sv
+++
@@ -1,8 +0,0 @@
-module Multiplexor (
-    input logic [63:0] a,
-    input logic [63:0] b,
-    input logic select,
-    output logic [63:0] result
-);
-    assign result = (select) ? b : a;
-endmodule
